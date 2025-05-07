53583048fd9d69c2ac1344cfe0aa603c1b5251db
gbtsui <gabriel.tsui@protonmail.com>
2025-05-07 11:49:47 -0400

    feat: basic homepage with some weird animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,48 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+import {motion, useScroll, useTransform} from "framer-motion";
+import {useRef, useState} from "react";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+export default function Home(){
+    const targetRef = useRef(null);
+
+    const {scrollYProgress} = useScroll({
+        target: targetRef,
+        offset: ["start start", "end start"],
+    });
+
+    const top_opacity = useTransform(scrollYProgress, [0, 0.9], [1, 0])
+    const section_1_opacity = useTransform(scrollYProgress, [0.9, 1], [1, 0])
+
+    console.log(top_opacity)
+
+    return (
+        <>
+            <div className={"relative"}>
+                <motion.div style={{opacity: top_opacity}} ref={targetRef} className={"top-0 h-screen flex items-center justify-center gap-7"}>
+                    <h1 className={"text-6xl"}>istopia</h1>
+                    <h2 className={"text-xl text-gray-300"}>write beyond your words.</h2>
+                    {/*
+                    SHOW ME THE SOUND DON'T LET IT FADE AWAYYYYYYYYYYYYYYYYYYYY
+                    I CAN SEE YOU CAME FROM FAR AWAY
+                    THE FACT THAT WE WOULD MEET IT MUST BE FATE
+                    FAR ACROSS THE OCEAN'S WATERS DREAMING FOR ESCAPE
+                    FAINTLY
+                    A HIDDEN LIGHT
+                    THAT IS LOST INSIDE YOUR EYES
+                    YEARNING FOR A CHANCE TO BURN BRIGHT
+                    */}
+                </motion.div>
+                <motion.div style={{opacity: section_1_opacity}} className={"sticky top-0 h-screen flex flex-row-reverse items-center justify-start bg-gray-900"}>
+                    <h3 className={"text-3xl"}>yet another platform for writers?</h3>
+                    <motion.div className={"p-6 m-3 w-[32rem] bg-gray-700 rounded-2xl"}>
+                        <p className={""}>
+                            words are everywhere. you can find writing platforms in every corner of the internet.
+                            what if you wanted something... bigger?
+                        </p>
+                    </motion.div>
+                </motion.div>
+            </div>
+        </>
+    )
 }
---
+++ src/app/tos/page.tsx
@@ -0,0 +1,50 @@
+export default function TOS(){
+    return (
+        <>
+            <div className={"text-center p-4"}>
+                <h1 className={"text-4xl"}>
+                    Terms of Service and Use
+                </h1>
+                <h2 className={"text-lg"}>
+                    does anyone ever even read these?
+                </h2>
+            </div>
+
+            <div className={"p-3"}>
+                <h3 className={"text-xl pl-5"}>
+                    Content Guidelines
+                </h3>
+                <p>
+                    you're free to write whatever you want. but not every story is appropriate to share here. istopia is
+                    meant for
+                    meaningful and respectful storytelling and that means we don't allow anything that doesn't fit that
+                    category.
+                    as a general rule, stuff that falls into these categories isn't allowed:
+                </p>
+                <ul className={"text-sm"}>
+                    <li>pornographic material (keep things mostly sfw!)</li>
+                    <li>hate speech</li>
+                    <li>degrading content</li>
+                    <li>excessively gory or bloody content</li>
+                </ul>
+            </div>
+
+            <h3>
+                Moderation Policies
+            </h3>
+            <p>
+                MODS HE POSTED SWER WORD!!!!! MODS!!!!<br/>
+                moderation team has the right to remove content that we believe breaks our policies. you can appeal
+                this.
+                (appeal form coming soon!)
+            </p>
+
+            <h3>
+                TBD
+            </h3>
+            <p>
+                honestly idk why i decided to write a TOS before doing ANYTHING ELSE
+            </p>
+        </>
+    )
+}
