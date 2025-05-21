764400632d757614e7b57efd09e48865d5e04aaa
gbtsui <gabriel.tsui@protonmail.com>
2025-05-20 22:20:39 -0400

    feat: reorderPage function

--- src/app/component/editor/state/zustand.ts
+++ src/app/component/editor/state/zustand.ts
@@ -10,6 +10,7 @@ interface EditorStore extends EditorProps {
     addPage: (newPage: Page) => void,
     addBlock: (page_number: number, newBlock: Block) => void,
     reorderBlock: (page_number: number, block_id: string, new_position: number) => void,
+    reorderPage: (page_number: number, new_position: number) => void,
 }
 
 export const useEditorStore = create<EditorStore>((set) => ({
@@ -50,11 +51,16 @@ export const useEditorStore = create<EditorStore>((set) => ({
         return set((state) => {
             const updatedPages = state.content.pages.map((page) => {
                 if (page.page_number === page_number) {
-                    const block = page.blocks.find((b) => b.block_id == block_id);
-                    if (!block) return page;
-                    const idx = page.blocks.indexOf(block);
-                    page.blocks.splice(idx, 1);
-                    page.blocks.splice(new_position, 0, block);
+                    const blockIndex = page.blocks.findIndex((b) => b.block_id === block_id);
+                    if (blockIndex === -1) return page;
+
+                    const updatedBlocks = [...page.blocks];
+                    const [block] = updatedBlocks.splice(blockIndex, 1);
+                    updatedBlocks.splice(new_position, 0, block);
+                    return {
+                        ...page,
+                        blocks: updatedBlocks
+                    }
                 }
                 return page;
             })
@@ -65,5 +71,22 @@ export const useEditorStore = create<EditorStore>((set) => ({
                 }
             }
         })
+    },
+
+    reorderPage: (page_number, new_position) => {
+        return set((state) => {
+            const pageIndex = state.content.pages.findIndex((p) => p.page_number === page_number);
+            if (pageIndex === -1) return {...state};
+            const updatedPages = [...state.content.pages];
+            const [page] = updatedPages.splice(pageIndex, 1);
+            updatedPages.splice(new_position, 0, page);
+            return {
+                content: {
+                    pages: updatedPages
+                }
+            }
+        })
     }
+
+
 }))
