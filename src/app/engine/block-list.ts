5d03cbac1773ce6936cc2f8fc8d3c028acc4e793
gbtsui <gabriel.tsui@protonmail.com>
2025-05-30 19:26:33 -0400

    feat: started shifting my engine paradigm
    - instead of refs, use engine-defined triggers and listeners

--- src/app/engine/block-list.ts
+++ src/app/engine/block-list.ts
@@ -1,20 +1,43 @@
-import {JSX} from "react";
+import {JSX, Ref} from "react";
 import {Block, BlockProps} from "@/app/types";
 import SimpleText from "@/app/engine/engine-components/simple-text";
+import Typewriter, {TypewriterHandle} from "@/app/engine/engine-components/typewriter";
 
-type BlockInfo = {
-    block_name: string;
-    block_component: (props: BlockProps) => JSX.Element;
-    block_props: BlockProps
+type BlockTrigger = {
+    listen_target_id: string,
+    action: string
 }
 
-export const BlockList: BlockInfo[] = [
+type BlockInfo<T extends BlockProps = BlockProps> = {
+    block_name: string;
+    block_component: (props: T) => JSX.Element;
+    block_props: T,
+}
+
+export const BlockList: BlockInfo<any>[] = [
     {
         block_name: "text",
         block_component: SimpleText,
         block_props: {
             content: [],
             className: ""
+        }
+    },
+    {
+        block_name: "typewriter",
+        block_component: Typewriter,
+        block_props: {
+            content: [],
+            className: "",
+            characterDelay: 25,
+            lineDelay: 1000,
+            manual: false,
+            onComplete: () => {},
+            ref: null,
+
+            triggers: {
+
+            }
         }
     }
 
--- src/app/engine/render-block.tsx
+++ src/app/engine/render-block.tsx
@@ -5,6 +5,7 @@ import {JSX} from "react";
 import SimpleText from "@/app/engine/engine-components/simple-text";
 import Typewriter from "@/app/engine/engine-components/typewriter";
 import SimpleContainer from "@/app/engine/engine-components/simple-container";
+import {BlockList} from "@/app/engine/block-list";
 
 export default function RenderBlock({block}: {block:Block}): JSX.Element | null {
     switch (block.type) {
@@ -17,4 +18,13 @@ export default function RenderBlock({block}: {block:Block}): JSX.Element | null 
         default:
             return null
     }
+}
+
+export function RenderBlockFromList({block}: {block:Block}) {
+    for (const blockInfo of BlockList) {
+        if (block.type === blockInfo.block_name) {
+            return blockInfo.block_component(block.props)
+        }
+    }
+    return null
 }
