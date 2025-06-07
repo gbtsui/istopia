6703950e5f05e22936940d2034b8e0e204db6174
gbtsui <gabriel.tsui@protonmail.com>
2025-06-07 16:03:42 -0400

    feat: (hopefully) functional publish form it took so much debugging

--- src/app/component/editor/editor-components/publish-form.tsx
+++ src/app/component/editor/editor-components/publish-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import {useEditorMetaDataStore, useEditorStore} from "@/app/component/editor/state/zustand";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {redirect} from "next/navigation";
 import UpdatePieceMetaData from "@/app/api/data/pieces/update-piece-metadata";
 
@@ -10,10 +10,11 @@ export function PublishButton() {
 
     return (
         <div>
-            <button onClick={() => setDialogIsOpen(true)} className={"bg-gray-800 text-white p-5 m-4 text-2xl rounded-3xl"}>
+            <button onClick={() => setDialogIsOpen(true)}
+                    className={"bg-gray-800 text-white p-5 m-4 text-2xl rounded-3xl"}>
                 Publish
             </button>
-            <PublishForm setDialogIsOpen={setDialogIsOpen} dialogIsOpen={dialogIsOpen} />
+            <PublishForm setDialogIsOpen={setDialogIsOpen} dialogIsOpen={dialogIsOpen}/>
         </div>
     )
 }
@@ -28,29 +29,35 @@ function PublishForm(props: PublishFormProps) {
     const publishContent = useEditorStore((state) => state.publishContent);
     const [loading, setLoading] = useState(false);
 
-    const {piece_id, author_id, author_name, title, slug, summary, published} = useEditorMetaDataStore()
+    const {piece_id, author_id, author_name, title, slug, summary, published, setData} = useEditorMetaDataStore()
 
     const [enteredTitle, setEnteredTitle] = useState<string>(title);
     const [enteredSummary, setEnteredSummary] = useState<string>(summary)
     const [enteredSlug, setEnteredSlug] = useState<string>(slug);
 
-    const form_action = (formData: FormData) => {
+    useEffect(() => {
+        setEnteredTitle(title);
+        setEnteredSummary(summary);
+        setEnteredSlug(slug);
+    }, [dialogIsOpen])
+
+    const publish = () => {
         const save_to_db = async () => {
             setLoading(true);
             await UpdatePieceMetaData({
                 username: author_name,
                 piece_id,
                 metadata: {
-                    title: formData.get("title") as string,
-                    slug: formData.get("slug") as string,
-                    summary: formData.get("summary") as string
+                    title: enteredTitle,
+                    slug: enteredSlug,
+                    summary: enteredSummary,
                 }
             });
             publishContent(
                 author_name,
                 piece_id,
             ).then((result) => {
-                result.success && redirect(`/u/${author_name}/${slug}`)
+                result.success && redirect(`/u/${author_name}/${enteredSlug}`)
             })
         }
         save_to_db().then(() => console.log("formed actioned"))
@@ -59,29 +66,31 @@ function PublishForm(props: PublishFormProps) {
     if (!dialogIsOpen) return null;
 
     return (
-        <div className={"fixed inset-0 items-center justify-center flex z-50 w-full h-full bg-black/75 text-white backdrop-blur-md"}>
+        <div
+            className={"fixed inset-0 items-center justify-center flex z-50 w-full h-full bg-black/75 text-white backdrop-blur-md"}>
             <div className={"w-3/4 m-3 p-3 bg-gray-700 rounded-2xl"}>
                 <div className={"w-full flex"}>
                     <button onClick={() => setDialogIsOpen(false)}
                             className={"p-2 bg-black text-white h-10 w-10 text-center items-center rounded-2xl justify-self-end"}>x
                     </button>
                 </div>
-                <div>
-                    <form action={form_action}>
-                        <span className={"text-2xl"}>finalize and publish</span>
-                        <div className={"text-right flex flex-row items-center justify-evenly"}>
-                            <div>
-                                <p>Title</p><br/>
-                                <span className={"mt-0 p-3 bg-gray-600 text-gray-200 rounded-xl"}>
+                <div className={"flex flex-col gap-5 space-y-6"}>
+                    <span className={"text-2xl"}>finalize and publish</span>
+                    <div className={"text-right flex flex-row items-center justify-evenly gap-3"}>
+                        <div className={"space-y-4"}>
+                            <p>Title</p>
+                            <span className={"mt-0 p-3 bg-gray-600 text-gray-200 rounded-xl"}>
                                     <input type={"text"} name={"title"} className={"bg-gray-500 rounded-md"}
-                                       value={enteredTitle} onChange={(e) => {
-                                            setEnteredTitle(e.target.value)
-                                       }}/></span>
-                            </div>
-                            <div>
-                                <p>Slug</p><br/>
-                                <span className={"mt-0 p-3 bg-gray-600 text-gray-200 rounded-xl"}>https://istopia.vercel.app{/*TODO: change to actual canonical url*/}/u/{author_name}/
-                                    <input type={"text"} className={"bg-gray-500 rounded-md"} value={enteredSlug} name={"slug"}
+                                           value={enteredTitle} onChange={(e) => {
+                                        setEnteredTitle(e.target.value)
+                                    }}/></span>
+                        </div>
+                        <div className={"space-y-4"}>
+                            <p>Slug</p>
+                            <span
+                                className={"mt-0 p-3 bg-gray-600 text-gray-200 rounded-xl"}>https://istopia.vercel.app{/*TODO: change to actual canonical url*/}/u/{author_name}/
+                                    <input type={"text"} className={"bg-gray-500 rounded-md"} value={enteredSlug}
+                                           name={"slug"}
                                            onChange={(e) => {
                                                setEnteredSlug(e.target.value
                                                    .toLowerCase()
@@ -90,9 +99,16 @@ function PublishForm(props: PublishFormProps) {
                                                    .replace(/-+/g, "-"))        // collapse dashes
                                            }}/>
                                 </span>
-                            </div>
                         </div>
-                    </form>
+                    </div>
+                    <div className={"w-full"}>
+                        <p>Summary</p>
+                        <textarea name={"summary"} rows={10} className={"bg-gray-500 w-3/4 rounded-md resize-none w-full"}
+                                  value={enteredSummary} onChange={(e) => {
+                            setEnteredSummary(e.target.value)
+                        }}/>
+                    </div>
+                    <button onClick={publish} disabled={loading} className={"bg-black text-xl rounded-2xl p-5"}>Publish!</button>
                 </div>
             </div>
         </div>
