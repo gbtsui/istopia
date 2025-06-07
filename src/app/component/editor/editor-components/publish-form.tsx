2a90cd5ce33dabc023f5f5ba60bb53ffbab01210
gbtsui <gabriel.tsui@protonmail.com>
2025-06-07 14:39:59 -0400

    feat: cleaned up publish form, added regexing for slug

--- src/app/component/editor/editor-components/publish-form.tsx
+++ src/app/component/editor/editor-components/publish-form.tsx
@@ -30,6 +30,10 @@ function PublishForm(props: PublishFormProps) {
 
     const {piece_id, author_id, author_name, title, slug, summary, published} = useEditorMetaDataStore()
 
+    const [enteredTitle, setEnteredTitle] = useState<string>(title);
+    const [enteredSummary, setEnteredSummary] = useState<string>(summary)
+    const [enteredSlug, setEnteredSlug] = useState<string>(slug);
+
     const form_action = (formData: FormData) => {
         const save_to_db = async () => {
             setLoading(true);
@@ -55,20 +59,38 @@ function PublishForm(props: PublishFormProps) {
     if (!dialogIsOpen) return null;
 
     return (
-        <div className={"fixed inset-0 items-center justify-center flex z-50 w-full h-full bg-black/75 backdrop-blur-md"}>
+        <div className={"fixed inset-0 items-center justify-center flex z-50 w-full h-full bg-black/75 text-white backdrop-blur-md"}>
             <div className={"w-3/4 m-3 p-3 bg-gray-700 rounded-2xl"}>
-                <button onClick={() => setDialogIsOpen(false)} className={"p-4 bg-black rounded-2xl"}>x</button>
+                <div className={"w-full flex"}>
+                    <button onClick={() => setDialogIsOpen(false)}
+                            className={"p-2 bg-black text-white h-10 w-10 text-center items-center rounded-2xl justify-self-end"}>x
+                    </button>
+                </div>
                 <div>
                     <form action={form_action}>
-                        <div>
-                            <span>Title</span><br/>
-                            <input type={"text"} name={"title"} className={"p-3 bg-gray-600 rounded-xl"} defaultValue={title} onChange={() => {}}/>
-                        </div>
-                        <div>
-                            <span>Slug</span><br/>
-                            <span>https://istopia.vercel.app{/*TODO: change to actual canonical url*/}/u/{author_name}/
-                                <input type={"text"} defaultValue={slug} name={"slug"} onChange={() => {}}/>
-                            </span>
+                        <span className={"text-2xl"}>finalize and publish</span>
+                        <div className={"text-right flex flex-row items-center justify-evenly"}>
+                            <div>
+                                <p>Title</p><br/>
+                                <span className={"mt-0 p-3 bg-gray-600 text-gray-200 rounded-xl"}>
+                                    <input type={"text"} name={"title"} className={"bg-gray-500 rounded-md"}
+                                       value={enteredTitle} onChange={(e) => {
+                                            setEnteredTitle(e.target.value)
+                                       }}/></span>
+                            </div>
+                            <div>
+                                <p>Slug</p><br/>
+                                <span className={"mt-0 p-3 bg-gray-600 text-gray-200 rounded-xl"}>https://istopia.vercel.app{/*TODO: change to actual canonical url*/}/u/{author_name}/
+                                    <input type={"text"} className={"bg-gray-500 rounded-md"} value={enteredSlug} name={"slug"}
+                                           onChange={(e) => {
+                                               setEnteredSlug(e.target.value
+                                                   .toLowerCase()
+                                                   .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+                                                   .replace(/\s+/g, "-")        // collapse whitespace
+                                                   .replace(/-+/g, "-"))        // collapse dashes
+                                           }}/>
+                                </span>
+                            </div>
                         </div>
                     </form>
                 </div>
