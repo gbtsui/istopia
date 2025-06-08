4c49577922f6a104f6164f1e15a312282057ef24
gbtsui <gabriel.tsui@protonmail.com>
2025-06-07 20:48:51 -0400

    feat: functional dropdown menu

--- src/app/component/universal/navbar.tsx
+++ src/app/component/universal/navbar.tsx
@@ -2,7 +2,7 @@
 
 import {getSession} from "next-auth/react";
 import Link from "next/link";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {Session} from "next-auth";
 import Image from "next/image";
 
@@ -22,11 +22,7 @@ export default function Navbar() {
         <div className={"sticky p-5 m-3 bg-gray-800 flex flex-row justify-end gap-10 items-center rounded-xl"}>
             <Link href="/browse">browse</Link>
             {session ?
-                <div className={"relative w-24 h-24"}>
-                    <Image
-                        src={session.user?.image ? session.user.image as string : "https://qwdqjithytndumgsklyb.supabase.co/storage/v1/object/public/pfp/default/default.png"}
-                        alt={"pfp"}  fill={true} className={"rounded-3xl"}/>
-                </div>
+                <ProfilePictureDropdown image_url={session.user?.image as string} username={session.user?.name as string}/>
                 :
                 <Link className={"p-3 bg-white text-black rounded-xl"} href="/signin">sign in</Link>
             }
@@ -35,3 +31,56 @@ export default function Navbar() {
 
 
 }
+
+type ProfilePictureDropdownProps = {
+    image_url: string,
+    username: string,
+}
+
+export function ProfilePictureDropdown(props: ProfilePictureDropdownProps) {
+    const [open, setIsOpen] = useState<boolean>(false);
+
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            console.log("click detected")
+            console.log(dropdownRef.current)
+            //console.log(dropdownRef.current.contains(event.target as Node))
+            if (dropdownRef.current &&
+                !dropdownRef.current.contains(event.target as Node)) {
+                setIsOpen(false);
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        console.log("event listener added")
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    }, [open]);
+
+    return (
+        <>
+            <button onClick={() => setIsOpen(!open)}>
+                <div className={"relative w-20 h-20 cursor-pointer"}>
+                    <Image
+                        src={props.image_url ? props.image_url : "https://qwdqjithytndumgsklyb.supabase.co/storage/v1/object/public/pfp/default/default.png"}
+                        alt={"pfp"} fill={true} className={"rounded-full object-cover"}/>
+                </div>
+            </button>
+            {open && (
+                <div className="absolute mt-24 min-w-[200px] bg-white border rounded-md shadow-lg z-50">
+                    <div className={"p-1 text-black"}>
+                        <Link href={`/u/${props.username}`}>profile</Link>
+                    </div>
+                    <div className={"p-1 text-black"}>
+                        <Link href={`/dashboard`}>dashboard</Link>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+
+}
+
+// profile
+// dashboard
+// settings
