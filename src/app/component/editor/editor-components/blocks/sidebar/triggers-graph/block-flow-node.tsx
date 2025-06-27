58e03b5b2c0585a5cd08b1b21e254056d780ae3c
gbtsui <gabriel.tsui@protonmail.com>
2025-06-27 19:01:27 -0400

    fix: prevent build error

--- src/app/component/editor/editor-components/blocks/sidebar/triggers-graph/block-flow-node.tsx
+++ src/app/component/editor/editor-components/blocks/sidebar/triggers-graph/block-flow-node.tsx
@@ -13,12 +13,12 @@ export default function BlockFlowNode(props: NodeProps<BlockFlowNodeProps>) {
         <div>
             <div>{props.data.friendly_name}</div>
             {props.data.events.map((event) => (
-                <Handle type={"source"} position={Position.Right} id={event}>
+                <Handle type={"source"} position={Position.Right} id={event} key={event}>
                     <div className={"handletext"}>{event}</div>
                 </Handle>
             ))}
             {props.data.actions.map((action) => (
-                <Handle type={"target"} position={Position.Left} id={action}>
+                <Handle type={"target"} position={Position.Left} id={action} key={action}>
                     <div className={"handletext"}>{action}</div>
                 </Handle>
                 ))}
